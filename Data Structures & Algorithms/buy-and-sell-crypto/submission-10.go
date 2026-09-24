3a7240d48156e19cbc6636dc4e66388cdf28ab6c
Bhav Kirti Sharma <52321903+bhavkirti@users.noreply.github.com>
2026-09-24 22:12:46 +0530

    Add: buy-and-sell-crypto - submission-10

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-10.go
@@ -0,0 +1,14 @@
+func maxProfit(prices []int) int {
+	maxProfit := 0
+	minBuy := math.MaxInt32
+
+	for _, sell := range prices {
+		if sell - minBuy > maxProfit {
+			maxProfit = sell - minBuy
+		} 
+		if sell < minBuy {
+			minBuy = sell
+		}
+	}
+	return maxProfit
+}
